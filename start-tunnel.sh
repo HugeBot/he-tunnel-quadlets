462afa14c1e7fb737b0f3574150c49e07140382e
Juan Luis Caro <juanluiscarobenito@gmail.com>
2025-05-28 11:42:49 +0200

    fix

--- start-tunnel.sh
+++ start-tunnel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-set -e
+set -e 
 
 modprobe sit
-ip tunnel add he-ipv6 mode sit remote $REMOTE_IPV4 local $LOCAL_IPV4 ttl 255
+ip tunnel add he-ipv6 mode sit remote "$REMOTE_IPV4" local "$LOCAL_IPV4" ttl 255
 ip link set he-ipv6 up
-ip -6 addr add $TUNNEL_IPV6 dev he-ipv6
-ip -5 route add $ROUTED_BLOCK dev he-ipv6
+ip -6 addr add "$TUNNEL_IPV6" dev he-ipv6
+ip -5 route add "$ROUTED_BLOCK" dev he-ipv6
 
 tail -f /dev/null
