92978b7aca0aeef70ae0c198c118506acb8b8bdd
Juan Luis Caro <juanluiscarobenito@gmail.com>
2025-05-28 11:40:22 +0200

    Update start-tunnel.sh

--- start-tunnel.sh
+++ start-tunnel.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-REMOTE_IPV4="216.66.80.30"
-LOCAL_IPV4="XXX.XXX.XXX.XXX"
-TUNNEL_IPV6="2001:XXXX:XXXX:XXXX::2/64"
-ROUTED_BLOCK="20001:XXXX:XXXX::/48"
-
 modprobe sit
 ip tunnel add he-ipv6 mode sit remote $REMOTE_IPV4 local $LOCAL_IPV4 ttl 255
 ip link set he-ipv6 up
